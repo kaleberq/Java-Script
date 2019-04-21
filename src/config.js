291b0dca36d117c55e7042cc04c2fc4728994d56
kalebe <kalebemisael@gmail.com>
2019-04-20 21:23:52 -0300

    ultimo commit

--- src/config.js
+++ src/config.js
@@ -1,9 +1,9 @@
 global.SALT_KEY = 'f5b99242-6504-4ca3-90f2-05e78e5761ef';
-global.EMAIL_TMPL = '<strong>{0}</strong>';
+global.EMAIL_TMPL = ' Olá, <strong>{0}</strong> Seja Bem Vindo à Node Store';
 
 module.exports = {
     connectionString: 'mongodb://kalebe:Aquariofilia1996@crud-shard-00-00-myzxy.gcp.mongodb.net:27017,crud-shard-00-01-myzxy.gcp.mongodb.net:27017,crud-shard-00-02-myzxy.gcp.mongodb.net:27017/test?ssl=true&replicaSet=Crud-shard-0&authSource=admin&retryWrites=true',
-    sendgridKey: 'TBD',
+    sendgridKey: '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',
     containerConnectionString: 'TBD'
 }
 
--- src/repositories/customer-repository.js
+++ src/repositories/customer-repository.js
@@ -8,3 +8,15 @@ exports.create = async(data) => {
     await customer.save()
 }
 
+exports.authenticate = async(data) => {
+    const res = await Customer.findOne({
+        email: data.email, 
+        password: data.password
+    });
+    return res;
+}
+
+exports.getById = async(id) => {
+    const res = await Customer.findById(id)
+    return res;
+}
